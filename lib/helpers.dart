d6d43f0c4ac142176a92d17b078619f2447ae668
Mayar Abo mahmood <mayar@futurecode-co.com>
2025-04-21 13:22:37 +0300

    change the package name..

--- lib/helpers.dart
+++ lib/helpers.dart
@@ -1,4 +1,4 @@
-import 'package:intl_phone_field/countries.dart';
+import 'package:intl_phone_field_for_aj/countries.dart';
 
 bool isNumeric(String s) => s.isNotEmpty && int.tryParse(s.replaceAll("+", "")) != null;
 
